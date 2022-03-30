b5abd8937db722595db4bd815800da93c0333d6d
Efratror <max.avontuur@hotmail.com>
2022-03-30 09:09:03 +0200

    Play button and sketch output
    - Replaced the existing extension.processing command with a sketchRunner encapsulated in it's own class.
    
    - Added start and stop buttons for execution a sketch
    
    - Piped the output of a sketch to a dedicated outputchannel

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,15 +1,17 @@
 import * as path from 'path';
 import * as vscode from 'vscode';
-const childProcess = require('child_process');
-const fs = require('fs')
 import {
 	LanguageClient,
 	LanguageClientOptions,
 	ServerOptions,
 	TransportKind
 } from 'vscode-languageclient';
+import { SketchRunner } from './sketchRunner';
 
 let client: LanguageClient;
+
+export let clientPath : string
+export let serverPath : string
 
 export function activate(context: vscode.ExtensionContext) {
 	
@@ -43,75 +45,35 @@ export function activate(context: vscode.ExtensionContext) {
 		clientOptions
 	);
 
-	let disposable = vscode.commands.registerCommand('extension.processing', () => {
-		// Running the Sketch entered in Extension Host
-		vscode.window.showInformationMessage(`Running Processing Sketch.!`);
-		try{
+	clientPath = context.asAbsolutePath(path.join('client'))
+	serverPath = context.asAbsolutePath(path.join('server'))
 
-			copyRecursiveSync(`${__dirname.substring(0,__dirname.length-11)}/server/out/compile/`, `${__dirname}/class`)
-			let workDir = `${__dirname.substring(0,__dirname.length-11)}/client/out/class`
-			let corePath = `${__dirname.substring(0,__dirname.length-11)}/server/src/processing/jar/core.jar`
-			let command
-			if (process.platform === 'win32') {
-				command =`java -cp ${corePath}; ProcessingDefault`
-				console.log("Windows environment, using -cp corePath \";\" as command")
-			}
-			else {
-				command = `java -cp ${corePath}: ProcessingDefault`
-				console.log("Linux/Mac environment, using -cp corePath \":\" as command")
-			}
-			childProcess.exec(command, {cwd: workDir})
-			
-		} catch(e) {
-			vscode.window.showInformationMessage(`Error occured while running sketch.!`);
-		}
+	let serverCompilePath = path.join(serverPath, 'out', 'compile')
+	let clientSketchPath = path.join(clientPath, 'out', 'class')
+	let processingCoreFile = path.join(serverPath, 'src', 'processing', 'jar', 'core.jar')
 
-	});
+	const sketchRunner = SketchRunner.getInstance();
+	sketchRunner.initilize(processingCoreFile, clientSketchPath, serverCompilePath)
+
+	let sketchRunnerDisp = vscode.commands.registerCommand("extension.processing.runSketch", () => sketchRunner.runSketch())
+	let sketchStopperDisp = vscode.commands.registerCommand("extension.processing.stopSketch", () => sketchRunner.stopSketch())
 
 	let referenceDisposable = vscode.commands.registerCommand('processing.command.findReferences', (...args: any[]) => {
 		vscode.commands.executeCommand('editor.action.findReferences', vscode.Uri.file(args[0].uri.substring(7,args[0].uri.length)), new vscode.Position(args[0].lineNumber,args[0].column));
 	})
 
-	context.subscriptions.push(disposable);
 	context.subscriptions.push(referenceDisposable)
+	context.subscriptions.push(sketchRunnerDisp)
+	context.subscriptions.push(sketchStopperDisp)
 
 	client.start();
 }
 
-export function deactivate(): Thenable<void> | undefined {
+export async function deactivate(): Promise<void> | undefined {
 	if (!client) {
 		return undefined;
 	}
+	const sketchRunner = SketchRunner.getInstance();
+	await sketchRunner.stopSketch();
 	return client.stop();
 }
-
-/**
- * Look ma, it's cp -R.
- * @param {string} src  The path to the thing to copy.
- * @param {string} dest The path to the new copy.
- */
-
- var copyRecursiveSync = function(src, dest) {
-
-	var exists = fs.existsSync(src);
-	var stats = exists && fs.statSync(src);
-	var isDirectory = exists && stats.isDirectory();
-
-	if (isDirectory) {
-		if(!fs.existsSync(src)) {
-	  		fs.mkdirSync(dest);
-		}
-
-	  fs.readdirSync(src).forEach(function(childItemName) {
-		copyRecursiveSync(path.join(src, childItemName),
-
-						  path.join(dest, childItemName));
-
-	  });
-
-	} else {
-	  fs.copyFileSync(src, dest);
-
-	}
-
-  };
